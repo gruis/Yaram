d829f45eaf6c2f09a9c28cddc59374269abb10bd
Caleb Crane <github@simulacre.org>
2011-08-22 23:08:50 +0900

    added basic support for encrypting and decrypting messages; added support for loading shared key; crypto library will only be used if a shared key is available

--- crypto/lib/yaram/crypto.rb
+++ crypto/lib/yaram/crypto.rb
@@ -1,26 +1,51 @@
 require "yaram"
+require "yaram/crypto/message"
 require "yaram/crypto/version"
-require "fast-aes"
 # Provided by MRI-1.9.2
 # @todo add check and fallback
 require "securerandom"
+# http://rubydoc.info/gems/fast-aes/0.1.1/frames
+# only provides cipher block chaining
+require "fast-aes"
 
 module Yaram
   module Crypto
     class << self
-
+      attr_writer :aes, :encoder
+      
       # Generates a random key that can be shared by Yaram actors
       # @todo make this better.
       def keygen(bits = 128)
         raise NotImplementedError unless defined?(SecureRandom)
         raise ArgumentError, "bits must be one of 128, 192, or 256" unless [128,192,256].include?(bits)
-        
-        return SecureRandom.uuid.gsub("-","") if bits == 128
-        return SecureRandom.uuid.gsub("-","") + SecureRandom.uuid.gsub("-","") if bits == 256
-        return SecureRandom.uuid.gsub("-","") + SecureRandom.uuid.gsub("-","")[0..15]
+        bytes = bits / 8
+        return SecureRandom.base64(bytes)[0...bytes]
       end # keygen
-
+      
+      def setup(path)
+        raise LoadError, "''#{path}' does not exist" unless File.exists?(path)
+        @encoder, Yaram.encoder = Yaram.encoder, self
+        @aes                    = FastAES.new(IO.read(path).chomp)
+      end # setup(path)
+      
+      def load(s)
+        o = @encoder.load(s)
+        return o unless o.is_a?(::Yaram::Crypto::Message)
+        # @todo detect failure and somehow notify the sender...
+        @encoder.load(@aes.decrypt(o.payload))
+      end # load(s)
+      
+      def dump(o)
+        message = Message.new(@aes.encrypt(@encoder.dump(o)))
+        # how does Yaram::Actor know to respond when the decryption fails?
+        message.from = o.from if o.respond_to?(:from)
+        @encoder.dump(message)
+      end # dump(o)
+      
     end # << self
-
+    
+    if File.exists?(keyfile = File.expand_path("~/.yaram/key"))
+      setup(keyfile)
+    end
   end # module::Crypto
 end # module::Yaram
--- crypto/spec/yaram-crypto_spec.rb
+++ crypto/spec/yaram-crypto_spec.rb
@@ -8,4 +8,15 @@ describe Yaram::Crypto do
     subject { Yaram::Crypto::VERSION }
     it { should be_a String }
   end # describe "VERSION"
+  
+  describe ".genkey" do
+    it "should only generate 128, 192, or 256 bit keys" do
+      expect { Yaram::Crypto.keygen(64) }.to raise_error(ArgumentError)
+      expect { Yaram::Crypto.keygen(32) }.to raise_error(ArgumentError)
+      expect { Yaram::Crypto.keygen(28) }.to raise_error(ArgumentError)
+      expect { Yaram::Crypto.keygen(128) }.to_not raise_error(ArgumentError)
+      expect { Yaram::Crypto.keygen(192) }.to_not raise_error(ArgumentError)
+      expect { Yaram::Crypto.keygen(256) }.to_not raise_error(ArgumentError)
+    end # should only generate 128, 192, or 256 bit keys
+  end # .genkey
 end # describe YaramCrypto
